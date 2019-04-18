31fb220afbc7e2adf75de03b8c50d2bc743f42cd
Ryan White <WHITER23@my.erau.edu>
2019-04-18 10:40:44 -0400

    test

---
+++ GarbageCollectionDemo/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollectionDemo
+{
+    class Calculator
+    {
+        // The purpose of this constructor is to enable you to verify that a Calculator
+        // object has been successfully created.
+        public Calculator()
+        {
+            Console.WriteLine("Calculator being created");
+        }
+
+        /* This destructor simply displays a message so that you can see when the
+        garbage collector runs and finalizes instances of this class. When writing
+        classes for real-world applications, you would not normally output text in a
+        destructor. */
+
+        ~Calculator()
+        {
+            Console.WriteLine("Calculator being finalized");
+        }
+
+        /* This is a very straightforward method that divides the first parameter by the
+        second and returns the result.It is provided just to add a bit of functionality
+        that can be called by an application.*/
+        public int Divide(int first, int second)
+        {
+            return first / second;
+        }
+    }
+}
---
+++ GarbageCollectionDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* This code creates a Calculator object, calls the Divide method of this object
+            (and displays the result), and then outputs a message as the program
+            finishes. */
+
+            Calculator calculator = new Calculator();
+            Console.WriteLine($"120 / 15 = {calculator.Divide(120, 15)}");
+            Console.WriteLine("Program finishing");
+        }
+    }
+}
